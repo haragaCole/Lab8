e7951a954e3549a7289d1048e45790bfebb8aae6
haragaCole <haraga@ualberta.ca>
2024-03-06 18:28:25 -0700

    has city implemented

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -67,7 +67,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public boolean hasCity(City city) {
-        return false;
+        return cities.contains(city);
     }
 //
 //    public int countCities() {
